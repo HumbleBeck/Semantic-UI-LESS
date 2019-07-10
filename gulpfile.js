a0c54627ad55d98cb72de083acc96a58ee2f16d0
HumbleBeck <humble.beck@gmail.com>
2019-07-10 22:24:00 +0300

    fix assets build

--- gulpfile.js
+++ gulpfile.js
@@ -35,12 +35,14 @@ const build = async () => {
   });
 
   await new Promise(resolve => {
-    gulp.src(`themes/**/assets/**/*`)
+    gulp.src('themes/**/assets/**/*.*')
       .pipe(rename((path) => {
         path.dirname = path.dirname.split('/').pop();
+        console.log(path);
+        
         return path;
       }))
-      .pipe(gulp.dest(path.join(dist, '.assets/')))
+      .pipe(gulp.dest(path.join(dist, 'assets/')))
       .on('end', resolve);
   });
 };
